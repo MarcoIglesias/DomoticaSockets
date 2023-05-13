e1edc88b28ccaf1a518431fad8fc3ab617a43ed8
MarcoIglesias <marco.iglesias.santos@alumnos.upm.es>
2023-05-13 08:49:18 +0200

    so

--- Domotica/Domotica/Views/VisualPage.xaml.cs
+++ Domotica/Domotica/Views/VisualPage.xaml.cs
@@ -10,6 +10,9 @@ using System.Xml.Serialization;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using System.Net;
+using System.Net.Sockets;
+
 
 namespace Domotica.Views
 {
@@ -18,7 +21,16 @@ namespace Domotica.Views
     {
 
         string IPaddress = "";
-        Client myclient;
+        //Client myclient;
+
+        //SOCKET INFO
+
+        IPHostEntry host;
+        IPAddress ipAdress;
+        IPEndPoint ipEndPoint;
+
+        Socket socket_client;
+        int port = 4044; //4044
         public VisualPage()
         {
             InitializeComponent();
@@ -26,56 +38,11 @@ namespace Domotica.Views
             if (Application.Current.Properties.ContainsKey("IP"))
             {
                 IPaddress = Application.Current.Properties["IP"].ToString();
-               // obtainInfo();
             }
-            else
-            {
-                IPaddress = "192.168.72.1";
-                //DisplayAlert("Conexión fallida","Por favor conéctese a un sistema","ok");
-            }
-            //init();
-            
 
-            // Server s = new Server("localhost", 4044);
-            //s.Start();  
         }
-        public VisualPage(Client c)
-        {
-            InitializeComponent();
-            myclient = c;
-
-            if (Application.Current.Properties.ContainsKey("IP"))
-            {
-                IPaddress = Application.Current.Properties["IP"].ToString();
-                //init(); // obtainInfo(myclient);
-                obtainInfo();
-            }
-            /*else
-            {
-                //DisplayAlert("Conexión fallida","Por favor conéctese a un sistema","ok");
-            }*/
-
-
-
-            // Server s = new Server("localhost", 4044);
-            //s.Start();  
-        }
-        public void init()
-        {
-            try
-            {                
-                Client c = new Client(IPaddress, 4044);
-                c.Start();
-                c.Send("init");
-                myclient = c;
-                DisplayAlert("Mandado", "holii", "OK");
-                obtainInfo();
-            }
-            catch(Exception ex) {
-                DisplayAlert("ERROR", ex.ToString(), "OK");
-            }
-            
-        }
+       
+        
         public void obtainInfo()
         {
             try
@@ -166,25 +133,173 @@ namespace Domotica.Views
                 DisplayAlert("Conexión fallida", "No se ha podido realizar conexión, compruebe la dirección IP", "Ok");
             }
 
-            //Thread.Sleep(5000);
-            //obtainInfo();
+
+        }
+
+        public void obtainInfo2(string msg)
+        {
+            //TempLabel.Text = msg + "ºC";
+            //HumLabel.Text = msg + "%";
+            string[] data = msg.Split(new string[] { " " }, StringSplitOptions.None);
+            TempLabel.Text = data[0] + "ºC";
+            HumLabel.Text = data[1] + "%";
+
+
+            if (data[2] == "1")
+            {
+                Ventilador.IsToggled = true;
+                Ventilador.ThumbColor = Color.Green;
+            }
+            else
+            {
+                Ventilador.IsToggled = false;
+                Ventilador.ThumbColor = Color.Red;
+            }
+
+
+            if (data[3] == "1")
+            {
+                LuzSalon.IsToggled = true;
+                LuzSalon.ThumbColor = Color.Green;
+            }
+            else
+            {
+                LuzSalon.IsToggled = false;
+                LuzSalon.ThumbColor = Color.Red;
+            }
+
+
+            if (data[4] == "1")
+            {
+                LuzCocina.IsToggled = true;
+                LuzCocina.ThumbColor = Color.Green;
+            }
+            else
+            {
+                LuzCocina.IsToggled = false;
+                LuzCocina.ThumbColor = Color.Red;
+            }
+
+
+            if (data[5] == "1")
+            {
+                LuzCuarto.IsToggled = true;
+                LuzCuarto.ThumbColor = Color.Green;
+            }
+            else
+            {
+                LuzCuarto.IsToggled = false;
+                LuzCuarto.ThumbColor = Color.Red;
+            }
+
+            if (data[6] == "1")
+            {
+                LuzGaraje.IsToggled = true;
+                LuzGaraje.ThumbColor = Color.Green;
+            }
+            else
+            {
+                LuzGaraje.IsToggled = false;
+                LuzGaraje.ThumbColor = Color.Red;
+            }
+
+            if (data[7] == "1")
+            {
+                PuertaGaraje.IsToggled = true;
+                PuertaGaraje.ThumbColor = Color.Green;
+            }
+            else
+            {
+                PuertaGaraje.IsToggled = false;
+                PuertaGaraje.ThumbColor = Color.Red;
+            }
+
+            //Thread.Sleep(1000);
+           /* Task.Delay(10000).Wait();
+            string newData = getData();
+            obtainInfo2(newData);*/
         }
 
         void BtnClicked(System.Object sender, System.EventArgs e)  //conectarse a la IP escrita
         {
+            //python
+
+            /* host = "localhost" # "192.168.1.61" # sys.argv[1]
+            port =  4044 #int(sys.argv[2])
+            
+            mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            mySocket.connect((host,port))
+            message1 = input("First num: ")
+            mySocket.send(message1.encode())
+            #message2 = input("Second num: ")
+            #mySocket.send(message2.encode())
+            
+            data = mySocket.recv(1024).decode()
+            print ('Received from server: ' + data)ç
+
+            */
             if (Application.Current.Properties.ContainsKey("IP"))
             {
-                //IPaddress = Application.Current.Properties["IP"].ToString();
-                 obtainInfo();
+
+                host = Dns.GetHostByName(IPaddress);
+                ipAdress = host.AddressList[0];
+                ipEndPoint = new IPEndPoint(ipAdress, port);
+
+                // socket = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                socket_client = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socket_client.Connect(ipEndPoint);
+
+
+                byte[] byteMsgSend = Encoding.ASCII.GetBytes("holiBoton");
+
+                socket_client.Send(byteMsgSend);
+
+
+                byte[] byteMsgRec = new byte[1024];
+                socket_client.Receive(byteMsgRec);
+                string msg = Encoding.ASCII.GetString(byteMsgRec);
+
+                obtainInfo2(msg);
             }
             else
             {
-                //obtainInfo(myclient);
+                
                 DisplayAlert("Conexión fallida", "Por favor conéctese a un sistema" + IPaddress, "ok");
             }
-           // obtainInfo();
         }
 
+        string getData()  
+        {
+            try
+            {
+                host = Dns.GetHostByName(IPaddress);
+                ipAdress = host.AddressList[0];
+                ipEndPoint = new IPEndPoint(ipAdress, port);
+
+                // socket = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                socket_client = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socket_client.Connect(ipEndPoint);
+
+
+                byte[] byteMsgSend = Encoding.ASCII.GetBytes("holiBoton");
+
+                socket_client.Send(byteMsgSend);
+
+
+                byte[] byteMsgRec = new byte[1024];
+                socket_client.Receive(byteMsgRec);
+                string msg = Encoding.ASCII.GetString(byteMsgRec);
+
+                return msg;
+            }
+            catch
+            {
+                return null;
+            }
+           
+        }
 
         /* //SOCKETS FUNCIONAL
             try
@@ -204,4 +319,4 @@ namespace Domotica.Views
                 DisplayAlert("Conexión fallida", "No se ha podido realizar conexión, compruebe la dirección IP", "Ok");
             }*/
     }
-}
+    }
